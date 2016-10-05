4462b1109e405b2e5c1cd6de28a13070de1952fd
oliveirabrunoa <oliveirabrunoa@gmail.com>
2016-10-05 16:59:15 -0300

    Authentication to ldap-ifba

--- teste.py
+++ teste.py
@@ -1,25 +1,24 @@
 from flask import Flask
 from flask import request
-from ldap3 import Server, Connection, ALL
+from ldap3 import Server, Connection, ALL, ALL_ATTRIBUTES, SUBTREE
+import json
 
 app = Flask(__name__)
 app.secret_key = 'dsajifjwq98f9qw8f98qw9fqwfkjqwofjw9qf89qw'
 
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.form.get('username')
+    email = request.form.get('email')
     password = request.form.get('password')
-    if not try_login(username, password):
-        return "Invalid credentials!"
-    return "Login successfullly"
-
-def try_login(username, password):
     try:
-        server = Server('ldap://ipa.demo1.freeipa.org:389', get_info=ALL)
-        Connection(server, 'uid={0}, cn=users, cn=accounts, dc=demo1, dc=freeipa, dc=org'.format(username), password, auto_bind=True)
-        return True
+        server = Server(host='10.1.0.4', get_info=ALL)
+        conn = Connection(server, user=email, password=password, auto_bind=True)
+        conn.search(search_base='DC=intranet, DC=cefetba, DC=br',search_filter='(sAMAccountName={0})'.format(str(email).split('@')[0]),search_scope=SUBTREE, attributes=ALL_ATTRIBUTES)
+        entry = conn.entries[0]
+        data = {'firstName': str(entry.givenName), 'lastName': str(entry.sn)}
+        return json.dumps(data)
     except:
-        return False
+        return json.dumps({"result":"Invalid credentials!"})
 
 if __name__=='__main__':
-  app.run()
+  app.run(debug=True)
